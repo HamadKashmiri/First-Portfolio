5302364a959317e2f4ff263f8ca068e632091380
Hamad <kashmirh@coventry.ac.uk>
2021-09-26 16:48:36 +0100

    exp

---
+++ src/components/Experience.js
@@ -0,0 +1,96 @@
+import React from 'react'
+
+const Experience = () => {
+    return (
+        <div className="experience">
+            <div className="d-flex mr-auto my-5">
+                <h1>Experience</h1> 
+            </div>
+            <div className="container experience-container">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>October 2021 - Ongoing</h3>
+                        <p>Working as a Software developer 
+                        at Space where I....
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem</p>
+                    </div>
+                </div>
+                {/*  */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>June 2021 - October 2021</h3>
+                        <p>First job as a Junior Software Engineer at Eventsforce.
+                        Used Intersystems Cache aswell as 
+                        React to assist building a huge events management system.
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem</p>
+                    </div>
+                </div>
+                {/*  */}
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>July 2019 - October 2019</h3>
+                        <p>lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem</p>
+                    </div>
+                </div>
+                {/*  */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>August 2018 - August 2020</h3>
+                        <p>Started coding professionally by doing Freelance work for Escape rooms
+                        and an arabic educational institution
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem</p>
+                    </div>
+                </div>
+                {/*  */}
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>May 2018 -  October 2019</h3>
+                        <p>Began working at a number of jobs including customer service, warehouse work,
+                        and tutoring. This experience allowed me to gain a number of key skills. 
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem
+                        lorem loren lorem lorem lorem</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Experience
