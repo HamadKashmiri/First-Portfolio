6f3bf3ae7d0cee7a8d255ca13f312fdd0e0d56b3
Hamad <kashmirh@coventry.ac.uk>
2021-09-26 13:57:41 +0100

    about added

---
+++ src/components/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import profile from '../profile.png'
+
+const About = () => {
+    return (
+        <div className="container py-5">
+            <div className="row">
+                <div className="col-lg-6 col-xm-12">
+                    <div className="profile-container">
+                        <img className="profile-img" src={profile} alt="profile"></img>
+                    </div>
+                    
+                </div>
+                <div className="col-lg-6 col-xm-12">
+                <h1 className="about-heading">About Me</h1>
+                    <p>
+                        Replace with something a describing me and what i do.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
